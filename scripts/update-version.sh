d677ffc20e204ce70888d6fb60f6f7e4365542f5
hyunseok.kim <dragmove@naver.com>
2023-04-14 03:32:36 +0900

    :construction: update files to test

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -12,23 +12,20 @@ if [ $IS_GITHUB_ACTIONS == 'true' ]; then
   git config --global user.name "${GITHUB_ACTOR_NAME}"
   git config --global user.email "${GITHUB_ACTOR_EMAIL}"
 
-  echo "GITHUB_ACTOR_NAME: ${GITHUB_ACTOR_NAME}"
-  echo "GITHUB_ACTOR_EMAIL: ${GITHUB_ACTOR_EMAIL}"
-
   # update version in package.json when branches are merged
   if [ "$GITHUB_BASE_REF" != 'main' ]; then
-    yarn plugin import version
+    # yarn plugin import version
     yarn version $INTEGRATED_VERSION
+
+    git add package.json .yarn
+    git commit -m ":bookmark: ${INTEGRATED_VERSION}"
+
+    PUSH_TARGET_BRANCH="HEAD:${GITHUB_BASE_REF}"
+
+    # echo "PUSH_TARGET_BRANCH: ${PUSH_TARGET_BRANCH}" #HEAD:epic/NOP-29
+
+    git tag $INTEGRATED_VERSION
+    git push origin $PUSH_TARGET_BRANCH --tags
   fi
-
-  git add package.json .yarn
-  git commit -m ":bookmark: ${INTEGRATED_VERSION}"
-
-  PUSH_TARGET_BRANCH="HEAD:${GITHUB_BASE_REF}"
-
-  echo "PUSH_TARGET_BRANCH: ${PUSH_TARGET_BRANCH}"
-
-  git tag $INTEGRATED_VERSION
-  git push origin $PUSH_TARGET_BRANCH --tags
 fi
 
